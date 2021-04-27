fb2c709b6ffd4a206ff84a7dde6b8114c6cd4169
Piotr Kosiński <kosinskipiotr.98@gmail.com>
2021-04-27 21:21:49 +0200

    Add variables

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var computerMove, item, playerMove;
+item = String(item) + String('');
+computerMove = 'kamień';
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+playerMove = 'papier';
+printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to kamień, to wygrywam!');
